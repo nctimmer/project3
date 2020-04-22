e8d98e3f613b78a28f61f0a8ebcfe81b3d46701d
nctimmer <60162672+nctimmer@users.noreply.github.com>
2020-04-22 13:55:18 -0400

    changing variable name

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function initMap() {
-	var el = new google.maps.Map(document.getElementById('map'));
+	var home = new google.maps.Map(document.getElementById('map'));
 	var myLocation = new google.maps.LatLng(73.9235, 40.7644);
-	
+	var mapOptions = {
+		center: myLocation
+		zoom: 15,
+		mapTypeId: google.maps.mapTypeId.SATELLITE
+		mapTypeControlOptions
+	}
 }
